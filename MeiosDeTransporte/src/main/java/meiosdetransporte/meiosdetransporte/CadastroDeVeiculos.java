9c4dd14c0175cd01462058a6acd6af1cd86591e5
Bruno Chim Silveira <101603545+brcsilveira@users.noreply.github.com>
2022-09-08 15:24:09 -0300

    Add files via upload

---
+++ MeiosDeTransporte/src/main/java/meiosdetransporte/meiosdetransporte/CadastroDeVeiculos.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package meiosdetransporte.meiosdetransporte;
+
+/**
+ *
+ * @author T-Gamer
+ */
+public class CadastroDeVeiculos {
+    private int[] listaDeVeiculos;
+    
+    public CadastroDeVeiculos(int tamanhoDaLista){
+        this.listaDeVeiculos[tamanhoDaLista] = listaDeVeiculos[tamanhoDaLista];
+    }
+    
+    
+}

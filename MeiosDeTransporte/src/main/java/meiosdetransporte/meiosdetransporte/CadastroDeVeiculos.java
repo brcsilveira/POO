5f105e25876def5df5a22d11636b3bc15d60227e
Bruno Chim Silveira <101603545+brcsilveira@users.noreply.github.com>
2022-09-08 20:29:31 -0300

    Inserção da classe "CadastroDeVeiculos".

--- MeiosDeTransporte/src/main/java/meiosdetransporte/meiosdetransporte/CadastroDeVeiculos.java
+++ MeiosDeTransporte/src/main/java/meiosdetransporte/meiosdetransporte/CadastroDeVeiculos.java
@@ -8,12 +8,55 @@ package meiosdetransporte.meiosdetransporte;
  *
  * @author T-Gamer
  */
-public class CadastroDeVeiculos {
-    private int[] listaDeVeiculos;
+public class CadastroDeVeiculos extends Veiculo{
+    private Veiculo[] listaDeVeiculos;
+    private int cont;
     
     public CadastroDeVeiculos(int tamanhoDaLista){
-        this.listaDeVeiculos[tamanhoDaLista] = listaDeVeiculos[tamanhoDaLista];
+        this.listaDeVeiculos = new Veiculo[tamanhoDaLista];
+        this.cont = 0;
     }
     
+    public void inserir(Veiculo objeto){
+        boolean flag = cont == listaDeVeiculos.length;
+        
+        if(flag){
+            Veiculo[] copy = new Veiculo[listaDeVeiculos.length * 2];
+            
+            System.arraycopy(this.listaDeVeiculos, 0, copy, 0, this.listaDeVeiculos.length);
+            
+            this.listaDeVeiculos = copy;
+        }
+        listaDeVeiculos[cont] = objeto;
+        cont++;
+    }
     
+    public String retornaItem (int posicao){
+        int index = posicao - 1;
+        
+        if(this.listaDeVeiculos[index] == null)
+            return null;
+        else{
+            return ("Veiculo da posição " + posicao + ": " + listaDeVeiculos[index]);
+        }
+    }
+    
+    public int tamanho(){
+        return cont;
+    }
+    
+    public void imprimir(){
+        for(int i = 0; i < tamanho(); i++){
+            if (this.listaDeVeiculos != null){
+                System.out.println(this.listaDeVeiculos[i]);
+            }
+            else{
+                System.out.println("NENHUM VEICULO CADASTRADO!");
+            }
+        }
+    }
+    
+    public void ordenar(){
+        
+    }
 }
